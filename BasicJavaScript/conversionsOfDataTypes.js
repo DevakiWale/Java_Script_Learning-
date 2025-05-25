3f0b23968c129db6f9e07de4344a306eb0d539a6
DevakiWale <138497864+DevakiWale@users.noreply.github.com>
2025-05-25 07:30:17 +0000

    Conversion of Data Type

---
+++ BasicJavaScript/conversionsOfDataTypes.js
@@ -0,0 +1,21 @@
+let score = "33abd";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score);
+// let valueNumber = String(score);
+// let valueNumber = Boolean(score);
+
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+
+// "33" => 33
+//"33qwe" => NaN
+// true => 1 false => 0
+
+
+// 1 => true ; 0 => false
+//""=> false
+//"deva"=> true
