771b9d916e7986b6e6fc3d7f22de61af80a86722
DevakiWale <138497864+DevakiWale@users.noreply.github.com>
2025-05-25 06:48:39 +0000

    Variables

---
+++ .github/Variables.js
@@ -0,0 +1,8 @@
+console.log("devaki");
+
+let accountAmount = 2345;
+const accountID = "Deva"
+accountState = "Pune";
+let accountCity;
+
+console.table([accountAmount, accountID , accountState, accountCity]);
